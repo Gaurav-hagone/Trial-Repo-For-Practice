fdc5d95c372d2538ad051d4a91642f65d02801da
Gaurav Hagone <142501350+Gaurav-hagone@users.noreply.github.com>
2024-02-06 22:57:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ console.log("Hello World", 4 + 6 , "Another log");
 console.warn("This is a warning");
 console.error("This is an error");
 console.log("Thala for a reason- 7")
+console.log("Thala for a reason- 07")
 
 
 
